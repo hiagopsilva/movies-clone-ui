1034d1d7295bf97a4c7c75bf2f0d1f60874446d3
hiagopsilva <hiagohps360@gmail.com>
2020-07-25 17:05:53 -0300

    add global

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${props => props.theme.colors.secundary};
+    font-size: 14px;
+    color: ${props => props.theme.colors.textPrimary};
+    font-family: Roboto, sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
