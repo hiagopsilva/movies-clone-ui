23307b670caf20c12638e95eab15b333aad32c26
hiagopsilva <hiagohps360@gmail.com>
2020-07-25 17:05:23 -0300

    add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#FFF',
+    secundary: '#F5F6F9',
+
+    background: '#FFF',
+
+    textPrimary: '#111011',
+    textSecundary: '#635E5E',
+  },
+};
