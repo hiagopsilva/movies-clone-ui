94d8f98d2af11bbbb877543b0add48abfee2ff2f
hiagopsilva <hiagohps360@gmail.com>
2020-07-25 20:22:27 -0300

    add dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,15 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#111011',
+    secundary: '#111011',
+
+    background: '#111011',
+    offColorSwitch: '#7159c1',
+    onColorSwitch: '#B3B1B7',
+
+    textPrimary: '#FFF',
+    textSecundary: '#635E5E',
+  },
+};
